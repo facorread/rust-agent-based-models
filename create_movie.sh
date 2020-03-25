346fbfd0bd2c4494b017918009b55bfa8781fd06
Fabio A. Correa Duran <facorread@gmail.com>
2020-03-25 15:50:17 -0400

    Comment about x265

--- create_movie.sh
+++ create_movie.sh
@@ -2,7 +2,8 @@
 
 if [[ -n "$(ls -1 img_dark/*.png 2>/dev/null)" ]]; then
   # Framerate: -r 20
-	ffmpeg -r 20 -i 'img_dark/%d.png' "animation$(date '+%F %Hh%Mm').mkv"
+  # Do NOT use x265; it takes more time and results in a larger file. -c:v libx265
+	ffmpeg -r 20 -i 'img_dark/%d.png' "animation$(date '+%FT%Hh%Mm').mkv"
 else
 	echo 'Please make sure there are png files in the img_dark/ folder.'
 fi
